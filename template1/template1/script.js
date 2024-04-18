3f38afa82d859c4d2f4ba637ae09c1021279b58b
Irakli <157306979+iraklius9@users.noreply.github.com>
2024-04-18 18:19:20 +0400

    Add files via upload

---
+++ template1/template1/script.js
@@ -0,0 +1,12 @@
+var list=[];
+list[0]="pirveli "
+list[1]="2 "
+list[2]="3 "
+list[3]="4 "
+list[4]="5 "
+list[5]="6 " 
+
+function pasuxi(I){
+ document.getElementById("paragraphi").innerHTML=
+ list[I-1]
+}
